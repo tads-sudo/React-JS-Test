237965e8ab7791f9d06acaa38947d090ff1f5a77
Mads97 <amadoviernes214@gmail.com>
2021-03-31 16:00:12 +0800

    Edited width of textfield in desktop.

--- src/components/Form/TextField/style.js
+++ src/components/Form/TextField/style.js
@@ -39,7 +39,7 @@ export const useStyles = createUseStyles({
   },
   [`@media(min-width:${SCREEN_DESKTOP.MIN}px)`]: {
     textField: {
-      width: "30vw",
+      width: "25vw",
     },
   },
 });
