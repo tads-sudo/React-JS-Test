3349a8da524d1a1c905e55410ba70ebf378d71ed
Mads97 <amadoviernes214@gmail.com>
2021-03-31 15:35:05 +0800

    Created Text Field component.

---
+++ src/components/Form/TextField/style.js
@@ -0,0 +1,45 @@
+import { createUseStyles } from "react-jss";
+import { color, SCREEN_TABLET, SCREEN_DESKTOP } from "../../../theme";
+
+export const useStyles = createUseStyles({
+  textField: {
+    width: "70vw",
+    "& input": {
+      color: color.BLACK,
+      background: "rgba(255, 255, 255, 0.05)",
+      borderRadius: "8px",
+      caretColor: color.TERTIARY,
+    },
+    "& label": {
+      color: color.GRAY,
+    },
+    "& label.Mui-focused": {
+      color: color.PRIMARY,
+    },
+
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: color.GRAY,
+        borderRadius: "8px",
+        transitionDuration: "0.2s",
+      },
+      "&:hover fieldset": {
+        borderColor: color.PRIMARY,
+        transitionDuration: "0.2s",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: color.PRIMARY,
+      },
+    },
+  },
+  [`@media(min-width:${SCREEN_TABLET.MIN}px)`]: {
+    textField: {
+      width: "50vw",
+    },
+  },
+  [`@media(min-width:${SCREEN_DESKTOP.MIN}px)`]: {
+    textField: {
+      width: "30vw",
+    },
+  },
+});
