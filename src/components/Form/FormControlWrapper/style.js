e8514638a472518034890aa0a4dfc7779e953d37
Mads97 <amadoviernes214@gmail.com>
2021-04-02 15:50:16 +0800

    Added width property in FormControlWrapper component

--- src/components/Form/FormControlWrapper/style.js
+++ src/components/Form/FormControlWrapper/style.js
@@ -1,9 +1,16 @@
 import { createUseStyles } from "react-jss";
+import { SCREEN_TABLET } from "../../../theme";
 
 export const useStyles = createUseStyles({
   FormControlWrapper: {
     display: "flex",
     flexDirection: "column",
+    width: "100%",
     marginBottom: "24px",
+  },
+  [`@media(min-width:${SCREEN_TABLET.MIN}px)`]: {
+    FormControlWrapper: {
+      width: "100%",
+    },
   },
 });
