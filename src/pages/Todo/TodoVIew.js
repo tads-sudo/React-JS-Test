884514798f518dc8aa296ecfaa696e20daaa8070
Mads97 <amadoviernes214@gmail.com>
2021-04-04 19:42:07 +0800

    Added content in Todo page

---
+++ src/pages/Todo/TodoVIew.js
@@ -0,0 +1,115 @@
+import React, { useMemo } from "react";
+import { useHistory } from "react-router-dom";
+
+import { useStyles } from "./style";
+
+import { useDispatch, useSelector } from "react-redux";
+import { logout, addTodo, deleteTodo } from "../../redux/actionCreators";
+
+import { Layout, Button, Form, Textfield } from "../../components";
+
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const TodoView = () => {
+  const classes = useStyles();
+
+  const dispatch = useDispatch();
+
+  const { auth, todos } = useSelector((state) => state);
+
+  const todosAsArray = useMemo(() => Object.values(todos.list), [todos]);
+
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+
+    dispatch(logout);
+
+    history.push("/login");
+  };
+
+  const todoSchema = Yup.object({
+    todo: Yup.string().required("Please input todo"),
+  });
+
+  const onSubmit = ({ todo }, helper) => {
+    dispatch(addTodo(todo));
+
+    helper.resetForm();
+  };
+
+  const initialValues = {
+    todo: "",
+  };
+
+  const {
+    values,
+    handleSubmit,
+    handleChange,
+    errors,
+    touched,
+    handleBlur,
+  } = useFormik({
+    onSubmit,
+    initialValues,
+    validationSchema: todoSchema,
+    validateOnChange: false,
+  });
+
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <Layout>
+      <div className={classes.container}>
+        <div className={classes.headingSection}>
+          <h1 className={classes.title}>{auth.userData.username} todo list</h1>
+          <Button onClick={handleLogout} buttonType="FILL" corner="8" size="SM">
+            Logout
+          </Button>
+        </div>
+        <Form onSubmit={handleSubmit}>
+          <div className={classes.inputAndButton}>
+            <Textfield
+              id="todo"
+              label="To do"
+              name="todo"
+              variant="outlined"
+              onChange={handleChange}
+              value={values.todo}
+              onBlur={handleBlur}
+              error={touched.todo && !!errors.todo}
+              helperText={touched.todo && errors.todo}
+            />
+            <Button buttonType="FILL" corner="8" size="XL" type="submit">
+              Add
+            </Button>
+          </div>
+        </Form>
+        <ul className={classes.todos}>
+          {todosAsArray.map((todo) => {
+            return (
+              <li key={todo.id} className={classes.todo}>
+                <p className={classes.todoText}>{todo.title}</p>
+                <Button
+                  buttonType="FILL"
+                  color="ERROR"
+                  size="SM"
+                  corner="8"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  Delete
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Layout>
+  );
+};
+
+export default TodoView;
