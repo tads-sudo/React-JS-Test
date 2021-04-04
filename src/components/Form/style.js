8835af84a339ed734075f7a744ef888917775b3f
Mads97 <amadoviernes214@gmail.com>
2021-04-04 19:23:04 +0800

    Removed SCREEN_TABLET and padding

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,17 +1,11 @@
 import { createUseStyles } from "react-jss";
-import { SCREEN_TABLET } from "../../theme";
 
 export const useStyles = createUseStyles({
   form: {
     display: "flex",
-    padding: "0 12px",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-  },
-  [`@media(min-width:${SCREEN_TABLET.MIN}px)`]: {
-    form: {
-      padding: "0",
-    },
+    width: "100%",
   },
 });
