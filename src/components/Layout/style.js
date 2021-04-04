9234e614f16db04690e136362b4fd422c34918b6
Mads97 <amadoviernes214@gmail.com>
2021-04-04 19:19:58 +0800

    Edited layout component style

--- src/components/Layout/style.js
+++ src/components/Layout/style.js
@@ -2,18 +2,19 @@ import { createUseStyles } from "react-jss";
 
 export const useStyles = createUseStyles({
   container: {
-    minHeight: "100vh",
-    padding: "0 0.5rem",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    height: "100vh",
+    padding: "0 0.5rem",
   },
   main: {
-    padding: "5rem 0",
-    flex: "1",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    width: "100%",
+    height: "100%",
+    padding: "5rem 0",
   },
 });
